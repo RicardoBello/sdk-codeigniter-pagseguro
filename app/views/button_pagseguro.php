46994f74d5c3ad8572c0c2b6321db5fc15775213
RicardoBello <ricco_bello1@hotmail.com>
2014-07-18 00:09:02 -0300

    app/views /button_page_pagseguro
    carrega o botao direcionando para o pagseguro enviando dados para a
    compra

---
+++ app/views/button_pagseguro.php
@@ -0,0 +1,27 @@
+<head>
+</head>
+<body>
+<div>
+    <div>
+        <h2>pagamento com PagSeguro</h2>
+        <br />
+        <br />
+        <div>
+            <table >
+                <tr>
+                    <td>
+                        <div >
+                            <!--
+                            aqui vai o botão pagseguro
+                            -->
+                            <?php echo $pagseguro; ?>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <br />
+    </div>
+</div>
+
+</body>
